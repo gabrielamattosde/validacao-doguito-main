03892c50b3768521d35b190e2d8ab537d37ac2eb
Gabriela Mattos <gabrielamattosde@gmail.com>
2022-05-13 12:15:12 -0300

    Criando validação de idade com javascript

---
+++ validacao-doguito-main/js/validacao.js
@@ -0,0 +1,30 @@
+const dataNascimento = document.querySelector('#nascimento');
+
+
+//O evento blur é acionado quando um elemento perde foco
+dataNascimento.addEventListener('blur', (evento) => {
+// Target uma referência ao objeto que enviou o evento
+    validaDataNascimento(evento.target);
+
+});
+
+function validaDataNascimento(input) {
+    const dataRecebida = new Date(input.value);
+    let mensagem = ''
+
+
+    if (!maiorQue18(dataRecebida)) {
+    mensagem = 'Você deve ser maior que 18 anos para se cadastrar.';
+    }
+
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCfullYear + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataMais18 <= dataAtual;
+
+
+}
